a571472f4ed440a3ead9f5d0bb0ea3f010e45b53
“TheKiller1959” <thekiller1959yt@gmail.com>
2022-06-24 14:11:29 -0500

    creación de test de integracion para usuarios

--- src/users/test/integration.test.js
+++ src/users/test/integration.test.js
@@ -75,14 +75,23 @@ describe('Suite de testing de integracion para Users', () => {
       })
   })
 
-  // it('Should reutn 200 when user information has deleted', (done) => {
-  //   chai.request(app)
-  //     .delete('/api/v1/users/530ef40f-1609-477e-88d9-6074df3900b6')
-  //     .set('Authorization', 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzMGVmNDBmLTE2MDktNDc3ZS04OGQ5LTYwNzRkZjM5MDBiNiIsImVtYWlsIjoianVhbmZAYWNhZGVtbG8uY29tIiwiaWF0IjoxNjU2MDkzNzE3fQ.fIAthzSWAeXl1EpUFbEUW55Wr32kJmBL43eMuYSM6pQ')
-  //     .end((err, res) => {
-  //       chai.assert.equal(res.status, 200)
-  //       chai.assert.typeOf(res.body, 'object')
-  //       done()
-  //     })
-  // })
+  it('Should reutn 202 when user information has deleted', (done) => {
+    chai.request(app)
+      .delete('/api/v1/users/1d88d376-81ba-4635-8b90-50b15af70c5b')
+      .set('Authorization', 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFkODhkMzc2LTgxYmEtNDYzNS04YjkwLTUwYjE1YWY3MGM1YiIsImVtYWlsIjoid2lsbWFyckBjYWRlbWxvLmNvbSIsImlhdCI6MTY1NjA5NzIzMH0.cjMwui9OILsxfuZHZmcHog7Nc5MXE39zjT1YqadrRB4')
+      .end((err, res) => {
+        chai.assert.equal(res.status, 202)
+        done()
+      })
+  })
+
+  it('Should reutn 400 when user information has no deleted', (done) => {
+    chai.request(app)
+      .delete('/api/v1/users/1d88d376-81ba-4635-8b90-50b15af70c5b')
+      .set('Authorization', 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFkODhkMzc2LTgxYmEtNDYzNS04YjkwLTUwYjE1YWY3MGM1YiIsImVtYWlsIjoid2lsbWFyckBjYWRlbWxvLmNvbSIsImlhdCI6MTY1NjA5NzIzMH0.cjMwui9OILsxfuZHZmcHog7Nc5MXE39zjT1YqadrRB4')
+      .end((err, res) => {
+        chai.assert.equal(res.status, 400)
+        done()
+      })
+  })
 })
