5c6418e43bc58d7de99feb162011ee7722468ac0
Jhoseb <jhoseb29@gmail.com>
2022-06-30 21:46:02 -0500

    pasandome a master

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const morgan = require("morgan");
 const config = require('./config')
 // Routers
-const  usersRouter  = require("./users/users.router").router
-const  authRouter  = require("./auth/auth.router").router
-const  conversationRouter  = require("./conversations/conversation.router").router
+const usersRouter = require("./users/users.router").router
+const authRouter = require("./auth/auth.router").router
+const conversationRouter = require("./conversations/conversation.router").router
 const messageRouter = require('./messages/message.router').router
 const participansRouter = require("./participants/participants.router").router
 
@@ -20,20 +20,29 @@ app.use(cors());
 app.use(express.json());
 
 // Enable incoming Form-Data
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 else app.use(morgan("combined"));
 
 // Endpoints
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to my API"
+    })
+})
 app.use("/api/v1", usersRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/", conversationRouter);
 app.use("/api/v1", messageRouter)
 app.use("/api/v1", participansRouter)
 
-app.listen(config.port, () =>{
+app.listen(config.port, () => {
     console.log(`Server started at port ${config.port}`)
 })
 
-module.exports = { app };
+module.exports = {
+    app
+};
